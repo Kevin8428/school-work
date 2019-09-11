bbc7f9b6fbb9a7e8507ee25c7f4026d24e351870
Kevin Deutscher <kevin@dispatch.me>
2019-09-10 21:03:36 -0400

    init

---
+++ data-structures/homework/01/Guess.java
@@ -0,0 +1,57 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Guess
+{
+    public static void main(String[] args)
+    {
+        int answer = getRandom(),
+            count = 0,
+            res = -1;
+        System.out.println("random gen num: "+ answer);
+        Scanner sc = new Scanner(System.in);
+
+        // Get user input. Test if it equal, higher or lower than answer.
+        // Malformated guesses (eg letters or numbers outside guess range)
+        // will not incrememt count. 
+        while (res != answer) {
+            int userInput = getUserInput(sc);
+            if (userInput == -1) {
+                System.out.println("input was of bad format");
+                sc.nextLine().equals("");
+            } else {
+                res = testGuess(userInput, answer);
+                count++;
+            }
+        }
+        System.out.println("Total guesses: " + count);
+    }
+    
+    // First check if input can be interpreted as an int.
+    // Then check if int is outside range. Return -1 if failure.
+    private static int getUserInput(Scanner sc){
+        System.out.println("input a number between 1 and 1000: ");
+        if (!sc.hasNextInt()) {
+            return -1;
+        }
+        int guess = sc.nextInt();
+        if (guess < 1 || guess > 1000) {
+            return -1;
+        }
+        return guess;
+    }
+
+    private static int testGuess(int n, int answer) {
+        if (n > answer) {
+            System.out.println("too high");
+        } else if (n < answer) {
+            System.out.println("too low");
+        }
+        return n;
+    }
+
+    private static int getRandom() {
+        Random r = new Random();
+        return r.nextInt(1001) + 0;
+    }
+}
---
+++ data-structures/homework/01/terms.md
@@ -0,0 +1,9 @@
+Lambda - first steps into functional programming for Java. You can now pass a functionality as an argument. A method will now accept an interface as a type. The lambda function doesn't need to belong to a class, can be executed on demand, and can be passed like an object.
+
+Static - This can be run before an instance of the class is instantiated. An example is the Main function in Java.
+
+Volatile - A java keyword used to make an execution thread safe; volatile will allow a method or class to be run by multiple an application with concurrency.
+
+Interface - An abstract type. A contract that says to become an interface (or satisfy the interface) the object must have certain method signatures. The interface can contain variables as well.
+
+Abstract - Similar to interfaces but it's a class which contains default method implementations. If you have an abstract class A and class B extends A, an instantiation of B inside A can't run B's methods unless A specifies the same abstract method.
